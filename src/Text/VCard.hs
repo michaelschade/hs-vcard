3286cabffec4cae6104e0549f0fcc4bd5ddf82cd
Michael Schade <michaelschade@sddomain.com>
2011-04-21 17:17:13 -0500

    CommonName and IndividualNames are required by RFC 2426.

--- src/Text/VCard.hs
+++ src/Text/VCard.hs
@@ -1,6 +1,8 @@
 module Text.VCard
     ( -- $doc
       VCard(..)
+    , CommonName
+    , IndividualNames
     , VCardProperty(..)
     , AddrType(..)
     , TelType(..)
@@ -17,97 +19,99 @@ import System.Locale (defaultTimeLocale)
 -- | Calling @show@ on @VCard@ will output a RFC 2426-compliant VCard that
 -- can, for example, be easily saved to a file and imported by any supporting
 -- program.
-data VCard      = VCard [VCardProperty]
+data VCard = VCard CommonName IndividualNames [VCardProperty]
+
+-- | Common name of the represented person. E.g.,
+--
+-- > CommonName "Mr. Michael A. F. Schade"
+type CommonName = String
+
+-- | A list of values corresponding, in sequence, to Family Name, Given
+-- Name, Additional Names, Honorific Prefixes, and Honorific Suffixes. E.g.,
+--
+-- > IndividualNames ["Schade", "Michael", "Anthony", "Fanetti", "Mr."]
+type IndividualNames = [String]
 
 data VCardProperty =
-    -- | Common name of the represented person. E.g.,
-    --
-    -- > CommonName "Mr. Michael A. F. Schade"
-      CommonName        String
-    -- | A list of values corresponding, in sequence, to Family Name, Given
-    -- Name, Additional Names, Honorific Prefixes, and Honorific Suffixes. E.g.,
-    --
-    -- > IndividualNames ["Schade", "Michael", "Anthony", "Fanetti", "Mr."]
-    | IndividualNames   [String]
     -- | A list of nicknames belonging to the VCard entity. E.g.,
     --
     -- > Nickname ["Mike", "Mikey"]
-    | Nickname          [String]
+      Nickname [String]
     -- | A photo of the VCard entity. E.g.,
     --
     -- > Photo (URI "http://accentuate.us/smedia/images/michael.jpg")
-    | Photo             { phtType   :: Maybe String -- ^ Registered IANA format
-                        , phtData   :: Data
-                        }
+    | Photo { phtType   :: Maybe String -- ^ Registered IANA format
+            , phtData   :: Data
+            }
     -- | Specifies the birth date of the VCard entity. E.g.,
     --
     -- > Birthday $ UTCTime (fromGregorian 1991 10 14) (secondsToDiffTime 0)
-    | Birthday          UTCTime
+    | Birthday UTCTime
     -- | A physical address associated with the vCard entity. E.g.,
     --
     -- > Address [AddrParcel, AddrPostal] "PO Box 935" "" "" "Fenton" "MO"
     -- >                                  "63026" "USA"
-    | Address           { addrType      :: [AddrType]
-                        , poBox         :: String
-                        , extAddress    :: String
-                        , streetAddress :: String
-                        , locality      :: String -- ^ City
-                        , region        :: String -- ^ State or Province
-                        , postalCode    :: String
-                        , countryName   :: String
-                        }
+    | Address   { addrType      :: [AddrType]
+                , poBox         :: String
+                , extAddress    :: String
+                , streetAddress :: String
+                , locality      :: String -- ^ City
+                , region        :: String -- ^ State or Province
+                , postalCode    :: String
+                , countryName   :: String
+                }
     -- | Formatted text about the delivery address. This is typically similar
     -- to the information in Address. E.g.,
     --
     -- > Label  [AddrParcel, AddrPostal]
     -- >        ["Michael Schade", "PO Box 935", "Fenton, MO 63026"]
-    | Label             { lblType   :: [AddrType]
-                        , label     :: [String] -- ^ Will be newline separated
-                        }
+    | Label { lblType   :: [AddrType]
+            , label     :: [String] -- ^ Will be newline separated
+            }
     -- | A telephone number for the VCard entity, as well as a list of
     -- properties describing the telephone number. E.g.,
     --
     -- > Telephone [TelCell, TelPreferred] "+1-555-555-5555"
-    | Telephone         { telType   :: [TelType]
-                        , number    :: String
-                        }
+    | Telephone { telType   :: [TelType]
+                , number    :: String
+                }
     -- | An email address for the VCard entity, including a list of properties
     -- describing it. E.g.,
     --
     -- > Email [EmailInternet, EmailPreferred] "hackage@mschade.me"
-    | Email             { emailType :: [EmailType]
-                        , email     :: String
-                        }
+    | Email { emailType :: [EmailType]
+            , email     :: String
+            }
     -- | Specifies the mailing agent the vCard entity uses. E.g.,
     --
     -- > Mailer "MichaelMail 4.2" -- Not a real mailing agent, unfortunately :(
-    | Mailer            String
+    | Mailer String
     -- | Represents the time zone of the vCard entity. E.g.,
     --
     -- > TZ (hoursToTimeZone (-6))
-    | TZ                TimeZone
+    | TZ TimeZone
     -- | Relates to the global positioning of the vCard entity. The value is
     -- (latitude, longitude) and must be specified as decimal degrees,
     -- preferably to six decimal places.
     --
     -- > Geo (37.386013, -122.082932)
-    | Geo               (Double, Double)
+    | Geo (Double, Double)
     -- | The VCard entity's job title or other position. E.g.,
     --
     -- > Title "Co-Founder"
-    | Title             String
+    | Title String
     -- | Specifies the role associated with the title. E.g.,
     --
     -- > Role "Anything"   -- For the co-founder, or
     -- > Role "Programmer" -- For someone the title "Research and Development"
-    | Role              String
+    | Role String
     -- | An image of the vCard entity's logo. This would typically relate to
     -- their organization. E.g.,
     --
     -- > Logo (URI "http://spearheaddev.com/smedia/images/logo-trans.png")
-    | Logo              { lgoType   :: Maybe String -- ^ Registered IANA format
-                        , lgoData   :: Data
-                        }
+    | Logo  { lgoType   :: Maybe String -- ^ Registered IANA format
+            , lgoData   :: Data
+            }
     -- | Indicates the vCard of an assistant or area administrator who is
     -- typically separately addressable. E.g.,
     --
@@ -118,20 +122,20 @@ data VCardProperty =
     -- > Agent (AgentVCard (VCard   [ CommonName "James Q. Helpful"
     -- >                            , Email [EmailInternet] "j@spearheaddev.com"
     -- >                            ]))
-    | Agent             AgentData
+    | Agent AgentData
     -- | The organization to which an entity belongs followed by organizational
     -- unit names. E.g.,
     --
     -- > Organization ["Spearhead Development, L.L.C.", "Executive"]
-    | Organization      [String]
+    | Organization [String]
     -- | General categories to describe the vCard entity. E.g.,
     --
     -- > Categories ["Internet", "Web Services", "Programmers"]
-    | Categories        [String]
+    | Categories [String]
     -- | A general note about the vCard entity. E.g.,
     --
     -- > Note "Email is the absolute best contact method."
-    | Note              String
+    | Note String
     -- | Specifies the identifier of the product that created this vCard. E.g.,
     --
     -- > ProductId "-//ONLINE DIRECTORY//NONSGML Version 1//EN"
@@ -140,47 +144,47 @@ data VCardProperty =
     -- use a method such as that specified for Formal Public Identifiers in ISO
     -- 9070 to assure that the text value is unique,\" but this module does not
     -- support that.
-    | ProductId         String
+    | ProductId String
     -- | Distinguishes the current revision from other renditions. E.g.,
     --
     -- > Revision $ UTCTime (fromGregorian 2011 04 16) (secondsToDiffTime 0)
-    | Revision          UTCTime
+    | Revision UTCTime
     -- | Provides a locale- or national-language-specific formatting of the
     -- formatted name based on the vCard entity's family or given name. E.g.,
     --
     -- > SortString "Schade"
-    | SortString        String
+    | SortString String
     -- | Specifies information in a digital sound format to annotate some
     -- aspect of the vCard. This is typically for the proper pronunciation of the
     -- vCard entity's name. E.g.,
     --
     -- > Sound  "BASIC"
     -- >        (URI "CID:JOHNQPUBLIC.part8.19960229T080000.xyzMail@host1.com")
-    | Sound             { sndType   :: Maybe String -- ^ Registered IANA format
-                        , sndData   :: Data
-                        }
+    | Sound { sndType   :: Maybe String -- ^ Registered IANA format
+            , sndData   :: Data
+            }
     -- | A value to uniquely identify the vCard. Please note well that this
     -- should be one of the registered IANA formats, but as of this time, this
     -- module does not support listing the UID type. E.g.,
     --
     -- > UID "19950401-080045-40000F192713-0052"
-    | UID               String
+    | UID String
     -- | A website associated with the vCard entity. E.g.,
     --
     -- > URL "http://spearheaddev.com/"
-    | URL               String
+    | URL String
     -- | Describes the general intention of the vCard owner as to how
     -- accessible the included information should be. E.g.,
     --
     -- > Class ClassConfidential
-    | Class             Class
+    | Class Class
     -- | Specifies a public key or authentication certificate associated with
     -- the vCard entity. E.g.,
     --
     -- > Key "x509" (Binary "dGhpcyBjb3VsZCBiZSAKbXkgY2VydGlmaWNhdGUK")
-    | Key               { keyType   :: Maybe String -- ^ Registered IANA format
-                        , keyData   :: Data
-                        }
+    | Key { keyType   :: Maybe String -- ^ Registered IANA format
+          , keyData   :: Data
+          }
 
 -- | Represents the various types or properties of an address.
 data AddrType   = AddrDomestic | AddrInternational | AddrPostal | AddrParcel
@@ -192,25 +196,27 @@ data TelType    = TelHome | TelMessage | TelWork | TelVoice | TelFax | TelCell
                 | TelPCS | TelPreferred
 
 -- | Represents the various types or properties of an email address.
-data EmailType  = EmailInternet | EmailX400 | EmailPreferred
+data EmailType = EmailInternet | EmailX400 | EmailPreferred
 
 -- | Represents the data associated with a vCard's Agent. This could be a URI
 -- to such a vCard or the embedded contents of the vCard itself.
-data AgentData  = AgentURI String | AgentVCard VCard
+data AgentData = AgentURI String | AgentVCard VCard
 
 -- | Represents the various types of data that can be included in a vCard.
 data Data = URI String | Binary String
 
 -- | Classifies the vCard's intended access level.
-data Class      = ClassPublic | ClassPrivate | ClassConfidential
+data Class = ClassPublic | ClassPrivate | ClassConfidential
 
 instance Show VCard where
-    show (VCard vps) = intercalate "\n"
-        ["BEGIN:vCard","VERSION:3.0", vps', "END:vCard"]
-        where vps' = intercalate "\n" . map show $ vps
+    show (VCard fn ns vps) =
+        intercalate "\n" ["BEGIN:vCard", "VERSION:3.0", vps', "END:vCard"]
+        where vps' = intercalate "\n" $
+                [ "FN:" ++ escape fn
+                , "N:" ++ (intercalate ";" . map escape) ns
+                ] ++ map show vps
 
 instance Show VCardProperty where
-    show (CommonName fn)    = "FN:" ++ escape fn
     show (Nickname nn)      = "NICKNAME:" ++ (intercalate "," . map escape) nn
     show (Photo t d)        = "PHOTO" ++ showType t ++ show d
     show (Birthday bd)      = "BDAY:" ++ fmtTime "%Y-%m-%d" bd
@@ -243,7 +249,6 @@ instance Show VCardProperty where
     show (URL u)            = "URL:" ++ escape u
     show (Class c)          = "CLASS:" ++ show c
     show (Key t k)          = "KEY" ++ showType t ++ show k
-    show (IndividualNames fn) = "N:" ++ (intercalate ";" . map escape) fn
 
 instance Show AddrType where
     show AddrDomestic       = "DOM"
